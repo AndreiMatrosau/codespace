a28aaf866bd795f1124a980cf40a9add6e67e1ae
Andrei Matrosau <113800529+AndreiMatrosau@users.noreply.github.com>
2024-03-07 12:47:14 +0000

    refactor

--- run-vm.sh
+++ run-vm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting VM ..."
-sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=output_rl9/rocky9.3 -cpu host -m 4G -smp 2 -enable-kvm -hda output_rl9/rocky9.3 -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &
+sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=$4 -cpu host -m 4G -smp 2 -enable-kvm -hda $4 -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &
 
 # Try SSH connection until successful or until 1 minute has passed
 echo "Try to connect via ssh ..."
 counter=0
-until ssh -o StrictHostKeyChecking=no -i $1 -p 3777 rocky@localhost 'bash -s' < $2 > output.json
+until ssh -o StrictHostKeyChecking=no -i $1 -p 3777 $5@localhost 'bash -s' < $2 > output.json
 do
   sleep 5
   counter=$((counter+1))
--- scripts/run-vm.sh
+++ scripts/run-vm.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+# run-vm.sh
+
+# Parse arguments
+while getopts k:s:o:i:u: flag
+do
+    case "${flag}" in
+        k) key=${OPTARG};;
+        s) script=${OPTARG};;
+        o) output=${OPTARG};;
+        i) image=${OPTARG};;
+        u) user=${OPTARG};;
+    esac
+done
+
 echo "Starting VM ..."
-sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=output_rl9/rocky9.3 -cpu host -m 4G -smp 2 -enable-kvm -hda output_rl9/rocky9.3 -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &
+if ! sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=$image -cpu host -m 4G -smp 2 -enable-kvm -hda $image -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &; then
+    echo "Failed to start VM. Exiting..."
+    exit 1
+fi
 
 # Try SSH connection until successful or until 1 minute has passed
 echo "Try to connect via ssh ..."
 counter=0
-until ssh -o StrictHostKeyChecking=no -i $1 -p 3777 rocky@localhost 'bash -s' < $2 > output.json
+until ssh -o StrictHostKeyChecking=no -i $key -p 3777 $user@localhost 'bash -s' < $script > $output
 do
   sleep 5
   counter=$((counter+1))
