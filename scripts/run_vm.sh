72f1bdeb78c60a26f920b13cd0f85a9e64f0a51a
Andrei Matrosau <113800529+AndreiMatrosau@users.noreply.github.com>
2024-03-11 12:46:11 +0000

    update

--- scripts/run_vm.sh
+++ scripts/run_vm.sh
@@ -40,18 +40,18 @@ do
         k) key=${OPTARG};;
         s) script=${OPTARG};;
         u) user=${OPTARG};;
-        # i) image=${OPTARG};;
+        i) image=${OPTARG};;
         
     esac
 done
 
 echo "Starting VM ..."
-sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=rocky9.3 -cpu host -m 4G -smp 2 -enable-kvm -hda rocky9.3 -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &
+sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -boot c -drive format=raw,id=drive1,if=none,file=$4 -cpu host -m 4G -smp 2 -enable-kvm -hda $4 -netdev user,id=usernet0,hostfwd=tcp::3777-:22 -device virtio-net-pci,netdev=usernet0 -display none -vga none &
 
 # Try SSH connection until successful or until 1 minute has passed
 echo "Try to connect via ssh ..."
 counter=0
-until ssh -o StrictHostKeyChecking=no -i $1 -p 3777 $user@localhost 'bash -s' < $script > output.json
+until ssh -o StrictHostKeyChecking=no -i $1 -p 3777 $3@localhost 'bash -s' < $2 > output.json
 do
   sleep 5
   counter=$((counter+1))
